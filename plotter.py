0ca1fc4af3929016d7634532dd7cdbeff1a800ba
urgen sherpa <7010051+sherpaurgen@users.noreply.github.com>
2020-09-15 08:44:47 +0545

    Update plotter.py
    added support for memory statistics graph

--- plotter.py
+++ plotter.py
@@ -3,61 +3,89 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 import matplotlib.style as style
 
+dfmem = pd.read_csv('/tmp/memfile.csv', index_col='TS', parse_dates=True)
 dfbw = pd.read_csv('/tmp/bwfile.csv', index_col='TS', parse_dates=True)
 dfcpu = pd.read_csv('/tmp/cpufile.csv', index_col='TS', parse_dates=True)
 dfloadavg = pd.read_csv('/tmp/loadavg.csv', index_col='TS', parse_dates=True)
 dfiops = pd.read_csv('/tmp/iotps.csv', index_col='TS', parse_dates=True)
 
-fig, axe = plt.subplots(6, 1, figsize=(20, 15))
+fig, axe = plt.subplots(7, 1, figsize=(20, 15))
 style.use('fivethirtyeight')
-# receive kbps
-axe[0].plot_date(dfbw.index, dfbw['rxkBs'], '-', linewidth=0.5)
-# upload kbps
-axe[1].plot_date(dfbw.index, dfbw['txkBs'], '--', linewidth=0.5)
+# conversion of memory from KB to MB
+dfmem['kbmemfree'] = dfmem['kbmemfree'].div(1024).round(2)
+dfmem['kbavail'] = dfmem['kbavail'].div(1024).round(2)
+dfmem['kbmemused'] = dfmem['kbmemused'].div(1024).round(2)
+dfmem['kbcached'] = dfmem['kbcached'].div(1024).round(2)
+dfmem['kbbuffers'] = dfmem['kbbuffers'].div(1024).round(2)
+dfmem['kbcommit'] = dfmem['kbcommit'].div(1024).round(2)
+
+# memory
+axe[0].plot_date(dfmem.index, dfmem['kbmemfree'], '--', linewidth=0.7, color="purple",
+                 label='kbmemfree')
+axe[0].plot_date(dfmem.index, dfmem['kbavail'], ':', marker='o', linewidth=1, markersize=1, color="#92A399",
+                 label='kbavail')
+axe[0].plot_date(dfmem.index, dfmem['kbmemused'], '--', linewidth=0.7, color="#000f64",
+                 label='kbmemused')
+axe[0].plot_date(dfmem.index, dfmem['kbcached'], '--', linewidth=0.7, color="#0f6400",
+                 label='kbcached')
+
+axe[0].plot_date(dfmem.index, dfmem['kbbuffers'], '--', linewidth=0.7, color="#fb5b97",
+                 label='kbbuffers')
+axe[0].plot_date(dfmem.index, dfmem['kbcommit'], '--', linewidth=0.7, color="#00f064",
+                 label='kbcommit')
+axe[0].legend(loc=2, fontsize=7)
 
 # cpu stats
-axe[2].plot_date(dfcpu.index, dfcpu['iowait'], '--',
+axe[1].plot_date(dfcpu.index, dfcpu['iowait'], '--',
                  linewidth=0.5, color='red', label='iowait%')
-axe[2].plot_date(dfcpu.index, dfcpu['usr'], '--', linewidth=0.7, color="#6c3376",
+axe[1].plot_date(dfcpu.index, dfcpu['usr'], '--', linewidth=0.7, color="#6c3376",
                  label='usr%')
-axe[2].plot_date(dfcpu.index, dfcpu['sys'], '--', linewidth=0.8, color='orange',
+axe[1].plot_date(dfcpu.index, dfcpu['sys'], '--', linewidth=0.8, color='orange',
                  label='sys%')
-axe[2].legend(loc=2, fontsize=7)
+axe[1].legend(loc=2, fontsize=7)
 
 # loadavg
-axe[3].plot_date(dfloadavg.index, dfloadavg['ldavg-1'], '--',
+axe[2].plot_date(dfloadavg.index, dfloadavg['ldavg-1'], '--',
                  linewidth=0.5, color='red', label='ldavg-1')
-axe[3].plot_date(dfloadavg.index, dfloadavg['ldavg-5'], '--',
+axe[2].plot_date(dfloadavg.index, dfloadavg['ldavg-5'], '--',
                  linewidth=0.5, color='green', label='ldavg-5')
-axe[3].plot_date(dfloadavg.index, dfloadavg['ldavg-15'], '--',
+axe[2].plot_date(dfloadavg.index, dfloadavg['ldavg-15'], '--',
                  linewidth=0.5, color='blue', label='ldavg-15')
-axe[3].legend(loc=2, fontsize=7)
+axe[2].legend(loc=2, fontsize=7)
 
 # blocked tasks
-axe[4].plot_date(dfloadavg.index, dfloadavg['blocked'], '--',
+axe[3].plot_date(dfloadavg.index, dfloadavg['blocked'], '--',
                  linewidth=0.5, color='red', label='blockedTasks')
-axe[4].plot_date(dfloadavg.index, dfloadavg['runq-sz'], '--',
+axe[3].plot_date(dfloadavg.index, dfloadavg['runq-sz'], '--',
                  linewidth=0.5, color='blue', label='runq-sz')
-axe[4].legend(loc=2, fontsize=7)
+axe[3].legend(loc=2, fontsize=7)
 
 # iops TPS
-axe[5].plot_date(dfiops.index, dfiops['tps'], '--', linewidth=0.7,
+axe[4].plot_date(dfiops.index, dfiops['tps'], '--', linewidth=0.7,
                  color='red', label='tps')
-axe[5].plot_date(dfiops.index, dfiops['rtps'], '-', linewidth=0.5,
+axe[4].plot_date(dfiops.index, dfiops['rtps'], '-', linewidth=0.5,
                  color='green', label='rtps')
-axe[5].plot_date(dfiops.index, dfiops['wtps'], '-', linewidth=0.5,
+axe[4].plot_date(dfiops.index, dfiops['wtps'], '-', linewidth=0.5,
                  color='blue', label='wtps')
-axe[5].legend(loc=2, fontsize=7)
+axe[4].legend(loc=2, fontsize=7)
+
+
+# receive kbps
+axe[5].plot_date(dfbw.index, dfbw['rxkBs'], '-', linewidth=0.5)
+# upload kbps
+axe[6].plot_date(dfbw.index, dfbw['txkBs'], '--', linewidth=0.5)
+
 
 # Label
 # axe[0].set_title('Down')
 # axe[1].set_title('Up')
-axe[0].set_ylabel('Down KBps', fontsize=7)
-axe[1].set_ylabel('Up KBps', fontsize=7)
-axe[2].set_ylabel('Wait%', fontsize=7)
-axe[3].set_ylabel('Loadavg', fontsize=7)
-axe[4].set_ylabel('Blocked', fontsize=7)
-axe[5].set_ylabel('tps', fontsize=7)
+axe[0].set_ylabel('MB', fontsize=7)
+axe[1].set_ylabel('cpu%', fontsize=7)
+axe[2].set_ylabel('Loadavg', fontsize=7)
+axe[3].set_ylabel('Blocked', fontsize=7)
+axe[4].set_ylabel('TPS', fontsize=7)
+axe[5].set_ylabel('Download KBps', fontsize=7)
+axe[6].set_ylabel('Upload KBps', fontsize=7)
 plt.xlabel('TimeStamp', fontsize=7)  # common x label
 
 # locating axis major tick
@@ -74,4 +102,3 @@ for ax in axe:
 
 plt.savefig('sys_report.pdf')
 # plt.show()
-
